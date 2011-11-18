576589eb36db09cf987b0b231e29d0bcdb96437f
Cory Richerson <crichers@spsu.edu>
2011-11-18 17:00:59 -0500

    5:00 update

---
+++ OS_PROJECT/OS_PROJECT/Memory/PageTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OS_PROJECT
+{
+    class PageTable
+    {
+        PageTableLocation[] table = new PageTableLocation[512];
+
+        public PageTable()
+        {
+            for (uint iterator = 0; iterator < 512; iterator++)
+            {
+                table[iterator].Frame = 257;
+                table[iterator].InMemory = false;
+                table[iterator].OwnerID = 31;
+            }
+        }
+
+        struct PageTableLocation
+        {
+            public uint Frame;
+            public bool InMemory;
+            public uint OwnerID;
+        }
+    }
+}
